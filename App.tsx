c5e93842ce303be2b4f279d905289f81bba2ebd5
Rayhan Beebeejaun <44209174+rayhanrsb@users.noreply.github.com>
2023-02-16 21:48:06 +0400

    create navigation structure

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./Navigation";
 
 export default function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const handleToggleLogin = () => {
+    setLoggedIn(!loggedIn);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigation loggedIn={loggedIn} />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
