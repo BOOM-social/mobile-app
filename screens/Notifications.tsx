21767a8eaf330bf9dffcfbc6ce39cd103206b434
Thomas Gehrmann <s6thgehr@gmail.com>
2023-02-22 18:02:26 +0100

    Add divider component

--- screens/Notifications.tsx
+++ screens/Notifications.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { useAuthStore } from "../utils/authentication";
+import { AntDesign } from "@expo/vector-icons";
+import Divider from "../components/Divider";
 
 export default function Notifications() {
   const toggleLogin = useAuthStore((state) => state.toggle);
@@ -12,6 +14,38 @@ export default function Notifications() {
           source={require("../assets/icon.png")}
         />
         <Text style={styles.followRequestsText}>karenne +10 others</Text>
+        <AntDesign name="right" size={22} color="gray" />
+      </View>
+      <Divider />
+      <Text style={styles.date}>New</Text>
+      <View style={styles.notificationPanel}>
+        <Image
+          style={styles.profilePicture}
+          source={require("../assets/icon.png")}
+        />
+        <Text style={styles.notificationDescription}>
+          <Text style={styles.profileName}>karenne</Text> liked your photo.
+        </Text>
+        <Image
+          style={styles.notificationImage}
+          source={require("../assets/icon.png")}
+        />
+      </View>
+      <Divider />
+      <Text style={styles.date}>Today</Text>
+      <View style={styles.notificationPanel}>
+        <Image
+          style={styles.profilePicture}
+          source={require("../assets/icon.png")}
+        />
+        <Text style={styles.notificationDescription}>
+          <Text style={styles.profileName}>kiero, zackjohn</Text> and 26 others
+          liked your photo.
+        </Text>
+        <Image
+          style={styles.notificationImage}
+          source={require("../assets/icon.png")}
+        />
       </View>
       <Button title="Logout" onPress={() => toggleLogin()} />
     </View>
@@ -22,24 +56,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "flex-start",
+    alignItems: "stretch",
     justifyContent: "flex-start",
-    padding: 16,
+    padding: 0,
   },
   title: {
     fontSize: 22,
     fontWeight: "500",
     marginBottom: 16,
+    marginLeft: 16,
   },
   followRequestPanel: {
     flexDirection: "row",
     alignItems: "center",
+    marginBottom: 32,
+    marginLeft: 16,
   },
   profilePicture: {
-    height: 60,
-    width: 60,
-    borderRadius: 30,
-    marginRight: 32,
+    height: 40,
+    width: 40,
+    borderRadius: 20,
   },
-  followRequestsText: {},
+  followRequestsText: {
+    marginHorizontal: 16,
+  },
+  date: {
+    fontWeight: "bold",
+    marginLeft: 16,
+    marginBottom: 16,
+  },
+  notificationPanel: {
+    marginHorizontal: 16,
+    marginVertical: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  profileName: { fontWeight: "bold" },
+  notificationDescription: {
+    marginHorizontal: 16,
+    flex: 4,
+    fontSize: 12,
+  },
+  notificationImage: {
+    height: 40,
+    width: 40,
+  },
 });
