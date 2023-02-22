aaefe6e121b4bd18d62eb0a87f8efd0bac435a50
Thomas Gehrmann <s6thgehr@gmail.com>
2023-02-22 17:02:43 +0100

    Implement title

--- screens/Notifications.tsx
+++ screens/Notifications.tsx
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Image } from "react-native";
 import { useAuthStore } from "../utils/authentication";
 
 export default function Notifications() {
   const toggleLogin = useAuthStore((state) => state.toggle);
   return (
     <View style={styles.container}>
-      <Text>Notifications page</Text>
-            <Button title="Logout" onPress={() => toggleLogin()} />
+      <Text style={styles.title}>Follow Requests</Text>
+      <View style={styles.followRequestPanel}>
+        <Image
+          style={styles.profilePicture}
+          source={require("../assets/icon.png")}
+        />
+        <Text style={styles.followRequestsText}>karenne +10 others</Text>
+      </View>
+      <Button title="Logout" onPress={() => toggleLogin()} />
     </View>
   );
 }
@@ -15,7 +22,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    padding: 16,
   },
+  title: {
+    fontSize: 22,
+    fontWeight: "500",
+    marginBottom: 16,
+  },
+  followRequestPanel: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  profilePicture: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+    marginRight: 32,
+  },
+  followRequestsText: {},
 });
